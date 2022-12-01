5653d8af01bd27842f145ce093bb02ac66771729
Megan <42215983+mlk621@users.noreply.github.com>
2022-12-01 15:44:09 -0600

    add hardware reference csv update to script

--- csv/converters/script.py
+++ csv/converters/script.py
@@ -5,8 +5,11 @@ Also automatically updates the individual topic csv files.
 
 """
 import pathlib
-from utils import create_website_csv
+from utils import create_website_csv, collate_topic
 
 root_dir = pathlib.Path.cwd()
 print("root_dir:", root_dir)
 website = create_website_csv(root_dir, save_topics=True)
+
+topic = 'hardware'
+hardware_references = collate_topic(topic, root_dir)
