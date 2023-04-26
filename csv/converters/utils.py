73b945c70a65327ef4d9eb265abfacff632127d0
ZachMassa <118857089+ZachMassa@users.noreply.github.com>
2023-04-26 09:42:11 -0500

    udpate to utils.py

--- csv/converters/utils.py
+++ csv/converters/utils.py
@@ -164,6 +164,10 @@ def collate_topic(topic, root_dir, save=True):
                                        bib_file='IonQ.bib', 
                                        save=True)
         IonQ['csv-file'] = 'IonQ'
+        all_references = pd.concat([all_references, IonQ], 
+                                   ignore_index=True)
+    all_references = all_references[['csv-file', 'title', 'author', 'journal', 
+                                     'publisher', 'url', 'doi', 'month', 'year']]
     if save:
         all_csv = topic+'-references.csv'
         all_references.to_csv(csv_dir.joinpath(all_csv), index=False)
